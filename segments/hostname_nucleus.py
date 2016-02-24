d02da76141edfe825eb4c4403146b62799139b28
Steve Ramage <sramage@nucleuslabs.com>
2016-02-24 11:35:13 -0800

    Fixed speed issue due to hostname checking on dev servers Updated .gitignore to ignore .idea project files for pycharm

--- segments/hostname_nucleus.py
+++ segments/hostname_nucleus.py
@@ -1,27 +1,28 @@
 SERVER_ICON = u'\uf0ae'
-
+# This is a nucleus specific development version with pretty icons and a much shoortened dev hostname
 def add_hostname_nucleus_segment(powerline):
-    from socket import gethostname
+    import os
     if powerline.args.colorize_hostname:
         from lib.color_compliment import stringToHashToColorAndOpposite
         from lib.colortrans import rgb2short
-        hostname = gethostname().upper()
+        # this is not the recommended way to get hostname but it is faster and this is a very specific case
+        hostname = os.getenv('HOST').upper()
         if hostname.startswith("DEV-DUTCH-"):
             host_prompt = ' %s %s' % (SERVER_ICON, hostname[10:12])
         else:
-            host_prompt = ' %s %s' % (SERVER_ICON, hostname.split('.')[0])
+            host_prompt = ' %s %s' % (SERVER_ICON, '%m')
         FG, BG = stringToHashToColorAndOpposite(hostname)
         FG, BG = (rgb2short(*color) for color in [FG, BG])
 
         powerline.append(host_prompt, FG, BG)
     else:
-        hostname = gethostname().upper()
-
+        # this is not the recommended way to get hostname but it is faster and this is a very specific case
+        hostname = os.getenv('HOST').upper()
         if hostname.startswith("DEV-DUTCH-"):
-            host_prompt = ' %s %s' % (SERVER_ICON, hostname[10:12].upper())
+            host_prompt = ' %s %s' % (SERVER_ICON, hostname[10:12])
             BG = Color.HOSTNAME_BG
         else:
-            host_prompt = ' %s %s' % (SERVER_ICON, hostname.split('.')[0])
+            host_prompt = ' %s %s' % (SERVER_ICON, '%m')
             BG = Color.USERNAME_ROOT_BG
 
         powerline.append(host_prompt, Color.HOSTNAME_FG, BG)
